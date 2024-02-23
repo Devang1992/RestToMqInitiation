4eda8313c3b1ac2c1bd184bb6513e57942f909ed
Devang Acharya <33769903+Devang1992@users.noreply.github.com>
2024-02-23 16:41:47 -0500

    test commits

---
+++ src/test/java/com/mqInitializer/MqMessageGeneration/controller/MessageInitiationCotrollerTest.java
@@ -0,0 +1,12 @@
+package com.mqInitializer.MqMessageGeneration.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageInitiationCotrollerTest {
+
+    @Test
+    void mqMessageInitiator() {
+    }
+}
---
+++ src/test/java/com/mqInitializer/MqMessageGeneration/service/MqServiceTest.java
@@ -0,0 +1,12 @@
+package com.mqInitializer.MqMessageGeneration.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MqServiceTest {
+
+    @Test
+    void mqServiceInitiator() {
+    }
+}
